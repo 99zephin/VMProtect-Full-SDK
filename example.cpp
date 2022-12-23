89e803cfb5be179651fad0d5d42be8174673c166
zephin <32177494+99zephin@users.noreply.github.com>
2022-12-23 19:56:42 +0000

    Create example.cpp

---
+++ example.cpp
@@ -0,0 +1,49 @@
+#include <Windows.h>
+#include <stdio.h>
+
+#include "../VMProtectLib.h"
+
+void TestMutation()
+{
+	VMProtect::BeginMutation("seq1");
+	printf("code in seq1\n");
+	VMProtect::End();
+}
+
+void TestProtection()
+{
+	if (VMProtect::IsProtected(VMProtect::FlagAll))
+		printf("protected\n");
+	else
+		printf("not protected\n");
+}
+
+void TestStrings()
+{
+	VMProtect vmp;
+	const char* str1 = VMProtect::String(vmp, "test1");
+	const char* str2 = VMProtect::String(&vmp, "test2");
+	const char* str3 = VMProtect::String("test3");
+	printf("str1: %s\n", str1);
+	printf("str2: %s\n", str2);
+	printf("str3: %s\n", str3);
+	VMProtect::Clean(vmp);
+	printf("clean\n");
+	printf("str1: %s\n", str1);
+	printf("str2: %s\n", str2);
+	printf("str3: %s\n", str3);
+}
+
+int main()
+{
+	while (1)
+	{
+		TestMutation();
+		TestProtection();
+		TestStrings();
+		printf("sleep\n\n");
+		Sleep(1000);
+	}
+	system("pause");
+	return 0;
+}
